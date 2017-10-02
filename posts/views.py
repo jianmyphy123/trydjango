690d0b0d0626fe95b3d9d94c5d86024e716f4879
jianmyphy123 <jianmyphy123@tutanota.com>
2017-10-02 16:24:20 +1100

    init

---
+++ posts/views.py
@@ -0,0 +1,28 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+def post_create(request):
+    return HttpResponse('<h1>Create</h1>')
+def post_detail(request): # retrieve
+    context = {
+        'title': 'Detail'
+    }
+    return render(request, 'index.html', context)
+def post_list(request):   # list items
+    context = {
+        'title': 'List'
+    }
+    # if request.user.is_authenticated():
+    #     context = {
+    #         'title': 'My User List'
+    #     }
+    # else:
+    #     context = {
+    #         'title': 'List'
+    #     }
+    return render(request, 'index.html', context)
+def post_update(request):
+    return HttpResponse('<h1>Update</h1>')
+def post_delete(request):
+    return HttpResponse('<h1>Delete</h1>')
